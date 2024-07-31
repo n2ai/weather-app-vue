736d88e5aba0f27ecb6fd2092a2d52ba86874539
n2ai <longhai2511@gmail.com>
2024-07-31 00:33:33 -0500

    Setup weather card

---
+++ project-FE/src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import Main from './components/Main.vue';
+</script>
+
+<template>
+  <div class="background_div">
+    <Main ></Main>
+  </div>
+  
+   
+</template>
+
+<style scoped>
+.background_div{
+  width: 100vh;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+</style>
---
+++ project-FE/src/components/Main.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { nextTick, reactive } from "vue";
+</script>
+
+<template>
+    <div class="weather_card">
+        <div class="weather_card_search"> 
+
+        </div>
+
+        <div class="weather_card_">
+
+        </div>
+
+        <div> 
+
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.weather_card{
+    width: 350px;
+    height: 500px;
+    background-color: cornsilk;
+    border-radius: 15px;
+    border-style: solid;
+    border-width: thin;
+}
+</style>
